c342b34d65327e1fb0ece8181f74b8e48f47c295
Bradley Kang <bkang@lcs.on.ca>
2021-10-04 15:07:49 -0400

    Initial Commit

---
+++ StaticWeatherView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StaticWeatherView
+//
+//  Created by Brad Kang on 2021-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StaticWeatherView/StaticWeatherViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticWeatherViewApp.swift
+//  StaticWeatherView
+//
+//  Created by Brad Kang on 2021-10-04.
+//
+
+import SwiftUI
+
+@main
+struct StaticWeatherViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
