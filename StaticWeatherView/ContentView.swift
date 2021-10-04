d67b72d76d58806555befd81a339de379a2d4e83
Bradley Kang <bkang@lcs.on.ca>
2021-10-04 15:33:11 -0400

    Added the first few HStacks for current temperature and conditions with an image as well as highs and lows

--- StaticWeatherView/ContentView.swift
+++ StaticWeatherView/ContentView.swift
@@ -11,9 +11,25 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
+                Text("Lakefield")
+                    .font(.title)
+                Text("19°C")
+                    .font(.largeTitle)
+                    .bold()
+                HStack {
+                    Text("Sunny")
+                    Image(systemName: "sun.max.fill")
+                }
+                .font(.title2)
+                HStack {
+                    Text("H: 23°")
+                    Text("L: 13°")
+                }
+                .font(.body)
                 
             }
         }
+        .navigationTitle("Weather")
     }
 }
 
