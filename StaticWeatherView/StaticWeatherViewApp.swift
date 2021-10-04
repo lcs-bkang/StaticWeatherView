6560360b191ff18837c951ec5424c20b11bfaa69
Bradley Kang <bkang@lcs.on.ca>
2021-10-04 15:20:43 -0400

    Created a navigation view and started with the VStack

--- StaticWeatherView/StaticWeatherViewApp.swift
+++ StaticWeatherView/StaticWeatherViewApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct StaticWeatherViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
